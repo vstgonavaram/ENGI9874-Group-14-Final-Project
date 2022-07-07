f1c33d574c7cbf8223dc5dc52a6abf868ee518e2
Kartik Uppal <kartikuppal25@gmail.com>
2022-07-06 22:12:02 -0230

    First commit build structure

---
+++ UniversityAdmissionManagemenAPI/src/main/java/com/UniversityAdmissionManagemenAPI/UniversityAdmissionManagemenApiApplication.java
@@ -0,0 +1,13 @@
+package com.UniversityAdmissionManagemenAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityAdmissionManagemenApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversityAdmissionManagemenApiApplication.class, args);
+	}
+
+}
---
+++ UniversityAdmissionManagemenAPI/src/test/java/com/UniversityAdmissionManagemenAPI/UniversityAdmissionManagemenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UniversityAdmissionManagemenAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityAdmissionManagemenApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
